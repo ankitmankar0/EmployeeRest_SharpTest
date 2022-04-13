1976cd1aaa0e44fbe853d1a35589a339c739deba
ankitmankar0 <ankitmankar012@gmail.com>
2022-04-13 15:42:41 +0530

    Retrieved all Details

---
+++ RestApiTest/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestApiTest
+{
+    public class Employee
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string salary { get; set; }
+    }
+}
